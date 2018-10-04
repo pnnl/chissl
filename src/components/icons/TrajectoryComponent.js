bcab07055a724c61d3afc83e85a840d1d09f31c9
Dustin Arendt <dustin.arendt@pnnl.gov>
2018-10-03 17:08:05 -0700

    Slice by start and end provided.

--- src/components/icons/TrajectoryComponent.js
+++ src/components/icons/TrajectoryComponent.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 import {VictoryLine} from 'victory'
 
-export const TrajectoryComponent = ({coordinates, start, end}) =>
+export const TrajectoryComponent = ({coordinates=[], start, end, domain}) =>
   <VictoryLine
     width={75}
     height={75}
     padding={5}
-    data={coordinates.slice(-10)}
+    data={coordinates.slice(start, end)}
     x={0}
     y={1}
     sortKey={(_, i) => i}
