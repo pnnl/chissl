5068a9d90f3b5fec52f58f10c7eb1237f7401663
Dustin Arendt <dustin.arendt@pnnl.gov>
2018-11-07 15:02:31 -0800

    Disabled the world geom. because this was causing performance issues.

--- react-chissl/src/components/icons/MapTrajectoryComponent.js
+++ react-chissl/src/components/icons/MapTrajectoryComponent.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react"
 
-import {geoPath, geoAlbersUsa} from 'd3-geo'
+import {geoPath, geoEqualEarth, geoAlbersUsa} from 'd3-geo'
 
-import world from '../../data/us-states.json'
+// import world from '../../data/world-110m.geo.json'
 
 const width = 150;
 const height = 100;
-
-const projection = geoAlbersUsa()
-  .fitSize([width, height], world);
-
-const path = geoPath()
-  .projection(projection);
 
 const stateStyle = {
   fill: '#f0f0f0',
@@ -22,28 +16,46 @@ const stateStyle = {
 const pathStyle = {
   fill: 'none',
   stroke: 'black',
-  strokeWidth: 3
+  strokeWidth: 1
 }
 
-const MapTrajectoryComponent = ({coordinates}) =>
-  <div style={{margin: 10}}>
+const MapTrajectoryComponent = ({coordinates}) => {
+  const features = {
+    type: 'Feature',
+    geometry: {type: 'LineString', coordinates}
+  };
+
+  const projection = geoEqualEarth()
+    .fitSize([width, height], features);
+
+  const path = geoPath()
+    .projection(projection);
+
+  return <div style={{margin: 10}}>
     <svg width={width} height={height}>
+      <text
+        x={5} y={15}
+        style={{fill: 'lightgray'}}
+      >
+        {coordinates.length}
+      </text>
+
+      {/*
+
       <g style={stateStyle}>
         {world.features.map((d, i) =>
           <path key={i} d={path(d)}/>
         )}
       </g>
 
+      */}
+
       <path
         style={pathStyle}
-        d={
-          path({
-            type: 'Feature',
-            geometry: {type: 'LineString', coordinates}
-          })
-        }
+        d={path(features)}
       />
     </svg>
   </div>
+}
 
 export {MapTrajectoryComponent}
