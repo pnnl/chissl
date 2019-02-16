fbb69d4181975783408795b4313cfe122429e04f
Dustin Arendt <dustin.arendt@pnnl.gov>
2019-02-15 16:50:43 -0800

    Added required packages to setup.

--- python-chissl/setup.py
+++ python-chissl/setup.py
@@ -17,4 +17,16 @@ setup(
     license = "BSD",
     packages=['chissl'],
     long_description=read('README'),
+    install_requires=[
+        'flask>=0.12.2',
+        'matplotlib>=3.0.1',
+        'networkx>=2.2',
+        'nltk>=3.2.4',
+        'numpy>=1.15.0',
+        'pandas>=0.23.0',
+        'pymongo>=3.4.0',
+        'scipy>=1.1.0',
+        'scikit-learn>=0.20.1',
+        'umap-learn>=0.3.6'
+    ],
 )
